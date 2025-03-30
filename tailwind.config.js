9b65ab88714f2c0b2a6594b987e05af681b6f41b
maia <maiam.unc@gmail.com>
2025-03-30 14:07:45 -0400

    Intial Creation

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  mode: 'jit',
+  purge: ['./*.html'], // Adjust paths as needed
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  colors: {
+    'apple-blue': '#0385FF',
+    'apple-light-blue': '#79B3ED',
+  },
+};
